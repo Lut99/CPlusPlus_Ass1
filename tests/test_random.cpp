fe9007874fd243f321fb40adc00b8d59daf4d806
Lut99 <mullertim99@gmail.com>
2020-02-26 20:23:56 +0100

    Did some groundwork for the random tests

---
+++ tests/test_random.cpp
@@ -0,0 +1,33 @@
+/* TEST RANDOM.cpp
+ *   by Lut99
+ *
+ * Created:
+ *   2/26/2020, 7:55:17 PM
+ * Last edited:
+ *   2/26/2020, 7:56:21 PM
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   This file provides a way to randomly test the code in assignment1.cpp.
+ *   To validate, we make use of the default date library available in C++.
+ *   The percentage printed if for the function it mentions the part of
+ *   tests that it succeeded.
+**/
+
+#include <iostream>
+#include <chrono>
+
+#include "../src/assignment1.hpp"
+
+using namespace std;
+
+
+void date_from_lib(int lib_date, int& years, int& months, int& days) {
+    
+}
+
+
+int main() {
+    
+}
